28bf172742794a5596d3a77136c57005007cd209
faraji miller <farajimiller@farajis-MacBook-Pro.local>
2025-02-05 13:56:29 -0500

    Updataed base path to samosa-selector

--- samosa-selector/vite.config.js
+++ samosa-selector/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/timetabled/',
+  base: '/samosa-selector/',
   server: {
     host: true,
     port: 5173,
